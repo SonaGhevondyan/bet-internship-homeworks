f6f316997014e9ad79829f14d4ccc86af09c9cb9
Sona97 <ghevondyans@gmail.com>
2017-04-22 20:05:53 +0400

    8th commit

---
+++ lesson-3-practice-hw/equals.js
@@ -0,0 +1,19 @@
+//Equals
+'use strict'
+function equals(arg1, arg2) {
+    if((arg1 === undefined || arg1 === null) && (arg2 === undefined || arg2 === null)) {
+        return true;
+    } else if((typeof arg1 !== "object" && typeof arg1 !== "function") && (typeof arg2 !== "object" && typeof arg2 !== "function")) {
+        return arg1 === arg2;
+    } else if(typeof arg1 !== typeof arg2) {
+        return false;
+    } else if(Object.getOwnPropertyNames(arg1).toString() !== Object.getOwnPropertyNames(arg2).toString()) {
+        return false;
+    }
+    for(prop in arg1) {
+        if(!equals(arg1[prop], arg2[prop])) {
+            return false;
+        }
+    }
+    return true;
+}
