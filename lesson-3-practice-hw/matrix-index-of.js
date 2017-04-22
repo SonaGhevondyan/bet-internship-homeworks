152003242cad9a9fe504bc73c3ac0c490d91c065
Sona97 <ghevondyans@gmail.com>
2017-04-22 18:37:50 +0400

    5th commit

---
+++ lesson-3-practice-hw/matrix-index-of.js
@@ -0,0 +1,12 @@
+//matrixIndexOf
+'use strict'
+function matrixIndexOf(matrix, item) {
+    for(let i = 0; i < matrix.length; i++) {
+        for(let j = 0; j < matrix[i].length; j++) {
+            if(matrix[i][j] === item) {
+                return [i, j];
+            }
+        }
+    }
+    return -1;
+}
